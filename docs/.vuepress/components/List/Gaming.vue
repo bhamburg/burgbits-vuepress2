0da7572db48e86e58244b54a9e9edb84d74bfbb1
Brian Hamburg <bhamburg@gmail.com>
2022-01-18 10:52:50 -0500

    added ratings to lists

--- docs/.vuepress/components/List/Gaming.vue
+++ docs/.vuepress/components/List/Gaming.vue
@@ -20,7 +20,6 @@
           </a>
         </td>
         <td style="text-align:center">{{ i.platform }}</td>
-        <td>{{ i.notes }}</td>
       </tr>
     </tbody>
   </table>
@@ -45,10 +44,21 @@
           </a>
         </td>
         <td style="text-align:center">{{ i.platform }}</td>
-        <td><span v-if="i.firstTime" title="First playthrough">✅</span></td>
-        <td><span v-if="i.completed" title="Completed 100%">💯</span></td>
         <td style="text-align:center">{{ i.dateFinished }}</td>
-        <td>{{ i.notes }}</td>
+        <td style="text-align:center; font-size:1.5em;">
+          <span v-if="i.firstTime" title="first playthrough">✓</span>
+        </td>
+        <td style="text-align:center; font-size:1.5em;">
+          <span v-if="i.completed" title="completed 100%">💯</span>
+        </td>
+        <td style="text-align:center; font-size:2em;">
+          <span v-if="i.rating == 5" title="5/5 stars, loved it">🤩</span>
+          <span v-if="i.rating == 4" title="4/5 stars, liked it">😄</span>
+          <span v-if="i.rating == 3" title="3/5 stars, it was fine">🙂</span>
+          <span v-if="i.rating == 2" title="2/5 stars, didn't like it">😒</span>
+          <span v-if="i.rating == 1" title="1/5 stars, hated it">😡</span>
+          <span v-if="!i.rating" title="didn't rate it">🤔</span>
+        </td>
       </tr>
     </tbody>
   </table>
@@ -70,7 +80,6 @@
           columns: [
             {key: 'title', name: 'Title'},
             {key: 'platform', name: 'Platform'},
-            {key: 'notes', name: 'Notes'}
           ],
           items: list.filter((game) => {
             return !game.yearFinished
@@ -83,10 +92,10 @@
           columns: [
             {key: 'title', name: 'Title'},
             {key: 'platform', name: 'Platform'},
-            {key: 'firstTime', name: '✅'},
-            {key: 'completed', name: '💯'},
             {key: 'dateFinished', name: 'Date Finished'},
-            {key: 'notes', name: 'Notes'}
+            {key: 'firstTime', name: 'First Time'},
+            {key: 'completed', name: '100%'},
+            {key: 'rating', name: "My Rating"},
           ],
           items: list.filter((game) => {
             return game.yearFinished == this.year;
@@ -98,9 +107,16 @@
       itemsSorted: function() {
         return this.items.sort((a, b) => {
           if (a[this.sortKey] && b[this.sortKey]) {
-            return this.sortOrder === 'desc'
-            ? b[this.sortKey].localeCompare(a[this.sortKey])
-            : a[this.sortKey].localeCompare(b[this.sortKey]);
+            if (typeof a[this.sortKey] === 'string') {
+              return this.sortOrder === 'desc'
+              ? b[this.sortKey].localeCompare(a[this.sortKey])
+              : a[this.sortKey].localeCompare(b[this.sortKey]);
+            } else {
+              console.log(a[this.sortKey], b[this.sortKey]);
+              return this.sortOrder === 'asc'
+              ? b[this.sortKey] - a[this.sortKey]
+              : a[this.sortKey] - b[this.sortKey];
+            }
           } else if (!a[this.sortKey]) {
             return this.sortOrder === 'desc' ? -1 : 1;
           } else if (!b[this.sortKey]) {
