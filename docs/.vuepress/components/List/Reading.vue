b00a3da11237d3ce3bd3cae9ba7817c1a8fa88a6
Brian Hamburg <bhamburg@gmail.com>
2022-02-16 16:53:42 -0500

    refactored lists to use Back4App backend (Parse)

--- docs/.vuepress/components/List/Reading.vue
+++ docs/.vuepress/components/List/Reading.vue
@@ -14,13 +14,13 @@
     <tbody>
       <tr :key="i" v-for="i in itemsSorted">
         <td>
-          <span v-if="!i.url">{{ i.title }}</span>
-          <a v-if="i.url" :href="i.url" target="_blank" rel="noopener noreferrer">
-            {{ i.title }}<span><svg class="external-link-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg><span class="sr-only">open in new window</span></span>
+          <span v-if="!i.attributes.url">{{ i.attributes.title }}</span>
+          <a v-if="i.attributes.url" :href="i.attributes.url" target="_blank" rel="noopener noreferrer">
+            {{ i.attributes.title }}<span><svg class="external-link-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg><span class="sr-only">open in new window</span></span>
           </a>
         </td>
         <td>
-          {{ i.author }}<span v-if="i.addAuthors">, {{ i.addAuthors }}</span>
+          {{ i.attributes.author }}<span v-if="i.attributes.addAuthors">, {{ i.attributes.addAuthors }}</span>
         </td>
       </tr>
     </tbody>
@@ -40,24 +40,24 @@
     <tbody>
       <tr :key="i" v-for="i in itemsSorted">
         <td>
-          <span v-if="!i.url">{{ i.title }}</span>
-          <a v-if="i.url" :href="i.url" target="_blank" rel="noopener noreferrer">
-            {{ i.title }}<span><svg class="external-link-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg><span class="sr-only">open in new window</span></span>
+          <span v-if="!i.attributes.url">{{ i.attributes.title }}</span>
+          <a v-if="i.attributes.url" :href="i.attributes.url" target="_blank" rel="noopener noreferrer">
+            {{ i.attributes.title }}<span><svg class="external-link-icon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg><span class="sr-only">open in new window</span></span>
           </a>
         </td>
         <td>
-          {{ i.author }}<span v-if="i.addAuthors">, {{ i.addAuthors }}</span>
+          {{ i.attributes.author }}<span v-if="i.attributes.addAuthors">, {{ i.attributes.addAuthors }}</span>
         </td>
         <td style="text-align:center">
-          {{ i.dateRead }}
+          {{ i.attributes.dateRead }}
         </td>
         <td style="text-align:center; font-size: 2em;">
-          <span v-if="i.rating == 5" title="5/5 stars, loved it">🤩</span>
-          <span v-if="i.rating == 4" title="4/5 stars, liked it">😄</span>
-          <span v-if="i.rating == 3" title="3/5 stars, it was fine">🙂</span>
-          <span v-if="i.rating == 2" title="2/5 stars, didn't like it">😒</span>
-          <span v-if="i.rating == 1" title="1/5 stars, hated it">😡</span>
-          <span v-if="!i.rating" title="didn't rate it">🤔</span>
+          <span v-if="i.attributes.rating == 5" title="5/5 stars, loved it">🤩</span>
+          <span v-if="i.attributes.rating == 4" title="4/5 stars, liked it">😄</span>
+          <span v-if="i.attributes.rating == 3" title="3/5 stars, it was fine">🙂</span>
+          <span v-if="i.attributes.rating == 2" title="2/5 stars, didn't like it">😒</span>
+          <span v-if="i.attributes.rating == 1" title="1/5 stars, hated it">😡</span>
+          <span v-if="!i.attributes.rating" title="didn't rate it">🤔</span>
         </td>
       </tr>
     </tbody>
@@ -65,7 +65,21 @@
 </template>
 
 <script>
-  import list from './data/books.json'
+  import Parse from 'parse/dist/parse.min.js';
+
+  Parse.initialize('nXKjWFWz0noFDDV5kX101uKB4nImJyIDSjqoVPFG', 'TyfKd1IfKTYN6UZrQx9nDOU97maRlaNmt0ClVE85');
+  Parse.serverURL = "https://parseapi.back4app.com/";
+
+  const Book = Parse.Object.extend("Book")
+  const query = new Parse.Query(Book)
+  query.limit(9999)
+  let list = []
+  try {
+    list = await query.find()
+  } catch(error) {
+    console.log(error)
+  }
+  
   export default {
     props: { 
       currently: Boolean,
@@ -82,7 +96,7 @@
             {key: 'authorLastFirst', name: 'Author(s)'},
           ],
           items: list.filter((book) => {
-            return !book.dateRead;
+            return !book.attributes.dateRead;
           })
         };
       } else if (this.year) {
@@ -96,7 +110,7 @@
             {key: 'rating', name: 'My Rating'}
           ],
           items: list.filter((book) => {
-            return book.yearRead == this.year;
+            return book.attributes.yearRead == this.year;
           })
         }
       };
@@ -104,20 +118,20 @@
     computed: {
       itemsSorted: function() {
         return this.items.sort((a, b) => {
-          if (a[this.sortKey] && b[this.sortKey]) {
-            if (typeof a[this.sortKey] === 'string') {
+          if (a.attributes[this.sortKey] && b.attributes[this.sortKey]) {
+            if (typeof a.attributes[this.sortKey] === 'string') {
               return this.sortOrder === 'desc'
-              ? b[this.sortKey].localeCompare(a[this.sortKey])
-              : a[this.sortKey].localeCompare(b[this.sortKey]);
+              ? b.attributes[this.sortKey].localeCompare(a.attributes[this.sortKey])
+              : a.attributes[this.sortKey].localeCompare(b.attributes[this.sortKey]);
             } else {
-              console.log(a[this.sortKey], b[this.sortKey]);
+              console.log(a.attributes[this.sortKey], b.attributes[this.sortKey]);
               return this.sortOrder === 'asc'
-              ? b[this.sortKey] - a[this.sortKey]
-              : a[this.sortKey] - b[this.sortKey];
+              ? b.attributes[this.sortKey] - a.attributes[this.sortKey]
+              : a.attributes[this.sortKey] - b.attributes[this.sortKey];
             }
-          } else if (!a[this.sortKey]) {
+          } else if (!a.attributes[this.sortKey]) {
             return this.sortOrder === 'desc' ? -1 : 1;
-          } else if (!b[this.sortKey]) {
+          } else if (!b.attributes[this.sortKey]) {
             return this.sortOrder === 'desc' ? 1 : -1;
           }
         })
--- docs/.vuepress/components/List/Running.vue
+++ docs/.vuepress/components/List/Running.vue
@@ -11,18 +11,32 @@
     </thead>
     <tbody>
       <tr :key="i" v-for="i in items">
-        <td>{{ i.name }}</td>
-        <td style="text-align:center">{{ i.date }}</td>
-        <td>{{ i.distance }}</td>
-        <td style="text-align:right">{{ i.time }}</td>
-        <td style="text-align:right">{{ i.pace }}</td>
+        <td>{{ i.attributes.name }}</td>
+        <td style="text-align:center">{{ i.attributes.date }}</td>
+        <td>{{ i.attributes.distance }}</td>
+        <td style="text-align:right">{{ i.attributes.time }}</td>
+        <td style="text-align:right">{{ i.attributes.pace }}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-  import list from './data/runs.json'
+  import Parse from 'parse/dist/parse.min.js';
+
+  Parse.initialize('nXKjWFWz0noFDDV5kX101uKB4nImJyIDSjqoVPFG', 'TyfKd1IfKTYN6UZrQx9nDOU97maRlaNmt0ClVE85');
+  Parse.serverURL = "https://parseapi.back4app.com/";
+
+  const Run = Parse.Object.extend("Run")
+  const query = new Parse.Query(Run)
+  query.limit(9999)
+  let list = []
+  try {
+    list = await query.find()
+  } catch(error) {
+    console.log(error)
+  }
+  
   export default {
     props: { 
       currently: Boolean,
@@ -30,8 +44,8 @@
     },
     data () {
       let sortedList = list.sort((a, b) => {
-        if (a.date && b.date) {
-          return b.date.localeCompare(b.date)
+        if (a.attributes.date && b.attributes.date) {
+          return b.attributes.date.localeCompare(b.attributes.date)
         }
       })
       return {
